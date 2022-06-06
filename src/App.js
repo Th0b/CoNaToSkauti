2c2d3938ec045ad6ff9b1d8ba84d0c6e1b9a1fa1
Th0be <navrat.tobias@gmail.com>
2022-06-06 19:29:43 +0200

    První nefunkční verze sdíleného stavu

--- src/App.js
+++ src/App.js
@@ -4,11 +4,37 @@ import {useState, useEffect} from 'react';
 export default function App() {
   const [questions, setQuestions] = useState([null]);
   const [loading, setLoading] = useState(true);
-  const [test, setTest] = useState(false);
+  const [status, setStatus] = useState("");
 
   useEffect(() => {
     getQuestions();
+    //synchronizace mezi okny - poslouchání a inicializace
+    setStatus(JSON.parse(localStorage.getItem("status") || ""));
+    window.addEventListener("storage", onStorageUpdate);
+    return () => {
+      window.removeEventListener("storage", onStorageUpdate);
+    };
   },[]);
+
+  //synchronizace mezi okny - update
+  const onStorageUpdate = (e) => {
+    const { key, newValue } = e;
+    if (key === "status") {
+      setStatus(JSON.parse(newValue));
+    }
+  };
+
+  //synchronizace mezi okny - test
+  const handleChange = (typ,e) => {
+    const newStatus = [...status];
+    if(typ==="body")
+      newStatus.body = e.target.value;
+    else if(typ==="otazka")
+      newStatus.otazka = e.target.value;
+    setStatus(newStatus);
+    console.log(status);
+    localStorage.setItem("status", JSON.stringify(e.target.value));
+  };
 
   const getQuestions = async () => {
     try{
@@ -24,8 +50,11 @@ export default function App() {
   return (
     <>
       <h1>Otázky</h1>
-      {!loading && questions.map(question => <p onClick={()=>setTest(!test)}>{question.otazka}</p>)}
-      <h2>{test?"Pravda":"Lež"}</h2>
+      {!loading && questions.map(question => <p>{question.otazka}</p>)}
+      <input value={status.body} placeholder="Body" onChange={(e) => handleChange("body",e)} />
+      <input value={status.otazka} placeholder="Otázka" onChange={(e) => handleChange("otazka",e)} />
+      <h5>Body: {status.body}</h5>
+      <h5>Otázka: {status.otazka}</h5>
     </>
   );
 }
